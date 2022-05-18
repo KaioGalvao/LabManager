f1c761c7f8d742d08d99c20e98411c7d8066c474
KaioGalvao <kaioninin@hotmail.com>
2022-05-18 09:12:20 -0300

    refactor: add model and repository for computer

---
+++ Models/Computer.cs
@@ -0,0 +1,15 @@
+namespace LabManager.Models;
+
+class Computer
+{
+    public int Id { get; set; }
+    public String Ram { get; set; }
+    public String Processor { get; set; }
+
+    public Computer(int id, string ram,string processor)
+    {
+        Id = id;
+        Ram = ram;
+        Processor = processor;
+    }
+}
