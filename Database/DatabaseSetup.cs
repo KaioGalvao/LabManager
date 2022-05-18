eb4b8db2f215d8f3071387889f8c094c54adc7aa
KaioGalvao <kaioninin@hotmail.com>
2022-05-18 08:33:14 -0300

    refactor: add database setup class

---
+++ Database/DatabaseSetup.cs
@@ -0,0 +1,58 @@
+using Microsoft.Data.Sqlite;
+
+namespace LabManager.Database;
+
+class DatabaseSetup{
+
+    public DatabaseSetup()
+    {
+        CreateComputerTable();
+        CreateLabTable();
+    }
+
+
+    private void CreateComputerTable()
+    {
+        var connection = new SqliteConnection("Data Source=database.db");
+
+        connection.Open();
+        var command = connection.CreateCommand();
+
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Computers(
+                ID int not null primary key,
+                ram int not null,
+                processor varchar(100) not null
+            );
+        ";
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
+
+    private void CreateLabTable()
+    {
+        var connection = new SqliteConnection("Data Source=database.db");
+
+        connection.Open();
+        var command = connection.CreateCommand();
+
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Labs(
+                ID INT NOT NULL PRIMARY KEY,
+                Number VARCHAR(100) NOT NULL,
+                Name VARCHAR(100) NOT NULL,
+                Block VARCHAR(50) NOT NULL
+            );
+        ";
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
+}
+
+
+
+
