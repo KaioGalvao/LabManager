f785a7562c882eedcc132cf473902319e9342749
KaioGalvao <kaioninin@hotmail.com>
2022-05-25 08:22:29 -0300

    refactor: added a save method, which saves computer values

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -47,4 +47,28 @@ class ComputerRepository
 
         return computers;
     }
+
+    public void Save(Computer computer)
+    {   
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+        var command = connection.CreateCommand();
+
+        command.CommandText = $"INSERT INTO Computers VALUES($id, $ram, $processor)";
+
+        command.Parameters.AddWithValue("$id", computer.Id);
+        command.Parameters.AddWithValue("$ram", computer.Ram);
+        command.Parameters.AddWithValue("$processor", computer.Ram);
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
+
+    /*
+    public void Save(int id, string ram, string processor)
+    {
+
+    }
+    */
 }
